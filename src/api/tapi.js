af91ab9cfed5339cb807742e2d209974df17e22d
Hitinbajaj <bajajhitin@gmail.com>
2023-07-29 19:59:50 +0530

    first commit

---
+++ src/api/tapi.js
@@ -0,0 +1,24 @@
+
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': 'd147d5ac39msha7378090fa5bd17p1b42eajsn153c0a0b1bec',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
